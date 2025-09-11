d1bf2a83253ca9ca7a9c31fbb8e13ff7d16d3ff6
han-nun <spshspshek@gmail.com>
2025-09-11 21:13:15 +0900

    feat: 공통 컴포넌트 드롭다운 추가 (#7)
    - 비즈니스 로직, 뷰 로직 분리
    - 임시 mock 데이터 추가 (dropdownOptions)

---
+++ src/constants/dropdown/dropdown.ts
@@ -0,0 +1,6 @@
+export const dropdownOptions = [
+  { label: 'CU 영등포점', value: 'cu-영등포점' },
+  { label: 'CU 여의나루역점', value: 'cu-여의나루역점' },
+  { label: 'CU 마포점', value: 'cu-마포점' },
+  { label: 'CU 강대점', value: 'cu-강대점' },
+]
