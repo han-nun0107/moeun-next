ddd5969663e734dc8f428900eb68340589666b49
han-nun <spshspshek@gmail.com>
2025-09-11 17:09:36 +0900

    feat: 공통 컴포넌트 Button 추가 (#5)
    - Button 컴포넌트 추가
    - Button Variant 추가
    - cn utils 추가

---
+++ src/foundations/button.ts
@@ -0,0 +1,32 @@
+export const BUTTON_VARIANTS = {
+  /* 로그아웃 버튼 */
+  VARIANTS1: [
+    'h-13',
+    'w-30',
+    'rounded-4xl',
+    'border',
+    'border-[#d9d9d9]',
+    'font-semibold',
+    'text-[#666666]',
+  ].join(' '),
+
+  /* 로그인 버튼 */
+  VARIANTS2: [
+    'h-13',
+    'w-41',
+    'rounded-[60px]',
+    'bg-[#f2544b]',
+    'text-center',
+    'leading-[52px]',
+    'font-semibold',
+    'text-white',
+    'hover:bg-[#e04439]',
+  ].join(' '),
+
+  /* 아이콘 버튼 */
+  ICON: '',
+} as const
+
+export const DEFAULT_BUTTON_VARIANT = 'VARIANTS1'
+
+export type ButtonVariant = keyof typeof BUTTON_VARIANTS
