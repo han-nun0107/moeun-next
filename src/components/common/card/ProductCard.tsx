31def68cb450f20cb393cd21a69757021ce07033
han-nun <spshspshek@gmail.com>
2025-09-17 20:54:25 +0900

    refactor: card 컴포넌트 수정 (#15)

--- src/components/common/card/ProductCard.tsx
+++ src/components/common/card/ProductCard.tsx
@@ -19,7 +19,13 @@ const ProductCard = ({
 }: ProductCardProps) => {
   return (
     <div className="flex flex-col gap-1">
-      <CardImage src={img} alt={alt} width={300} height={290} isLiked={true} />
+      <CardImage
+        src={img}
+        alt={alt}
+        width={300}
+        height={290}
+        showHeart={true}
+      />
       <h3 className="mt-4 text-left text-lg font-bold text-[#333]">{title}</h3>
       <p className="text-left text-base text-[#666]">{subtitle}</p>
       <p className="text-left text-base font-bold text-[#333]">{price}</p>
