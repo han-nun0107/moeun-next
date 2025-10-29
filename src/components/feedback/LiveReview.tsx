78ed812093b7c7819ce2cb7d4d5293a34815efe7
han-nun <spshspshek@gmail.com>
2025-10-29 22:37:50 +0900

    refactor: 코드리뷰 변경 (#43)
    - refetchBtn 이미지 최적화

--- src/components/feedback/LiveReview.tsx
+++ src/components/feedback/LiveReview.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 import refetchBtn from '@/assets/icons/review/refetch_btn.svg'
 import { Button, Card } from '@/components'
 import { liveReviewData } from '@/mocks/review/review'
@@ -15,7 +17,12 @@ const LiveReview = () => {
             한 잔 취향을 이용한 고객님들의 실시간 후기
           </p>
           <Button variant="ICON" aria-label="실시간 후기 새로고침">
-            <img src={refetchBtn.src} alt="실시간 후기 새로고침" />
+            <Image
+              src={refetchBtn.src}
+              alt="실시간 후기 새로고침"
+              width={40}
+              height={40}
+            />
           </Button>
         </div>
 
--- src/components/feedback/SimilarReview.tsx
+++ src/components/feedback/SimilarReview.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+
 import refetchBtn from '@/assets/icons/review/refetch_btn.svg'
 import { Card, Button } from '@/components'
 import { liveReviewData } from '@/mocks/review/review'
@@ -10,7 +12,12 @@ const SimilarReview = () => {
         <div className="flex w-full items-center justify-between gap-2">
           <p>한 잔 취향을 이용한 고객님들의 실시간 후기</p>
           <Button variant="ICON" aria-label="나와 비슷한 취향의 후기 새로고침">
-            <img src={refetchBtn.src} alt="나와 비슷한 취향의 후기 새로고침" />
+            <Image
+              src={refetchBtn.src}
+              alt="나와 비슷한 취향의 후기 새로고침"
+              width={40}
+              height={40}
+            />
           </Button>
         </div>
         <div className="mt-[50px] mb-25 grid min-h-[320px] grid-cols-4 gap-8">
