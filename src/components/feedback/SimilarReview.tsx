7f027355e93e8f76b02586fdc0c3bb8a9ab53c0f
han-nun <spshspshek@gmail.com>
2025-10-30 22:21:19 +0900

    refactor: 아이콘 변경 (#45)
    - RetryIcon 컴포넌트 추가 및 사용

--- src/components/feedback/SimilarReview.tsx
+++ src/components/feedback/SimilarReview.tsx
@@ -1,7 +1,4 @@
-import Image from 'next/image'
-
-import refetchBtn from '@/assets/icons/review/refetch.svg'
-import { Card, Button } from '@/components'
+import { Card, Button, RetryIcon } from '@/components'
 import { liveReviewData } from '@/mocks/review/review'
 
 const SimilarReview = () => {
@@ -13,12 +10,7 @@ const SimilarReview = () => {
           <p>한 잔 취향을 이용한 고객님들의 실시간 후기</p>
           <Button variant="ICON" aria-label="나와 비슷한 취향의 후기 새로고침">
             <div className="flex h-10 w-10 items-center justify-center rounded-full border border-[#d9d9d9] bg-[#fff]">
-              <Image
-                src={refetchBtn.src}
-                alt="실시간 후기 새로고침"
-                width={20}
-                height={20}
-              />
+              <RetryIcon className="h-5 w-5 text-[#333]" />
             </div>
           </Button>
         </div>
