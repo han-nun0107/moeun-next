c9571f76443f2fa4d056c4aef1d25268780f7d0a
han-nun <spshspshek@gmail.com>
2025-11-02 17:24:12 +0900

    feat: 좋아요 페이지 구현 (#53)
    - 좋아요 페이지 구현
    - 메타데이터 추가

--- src/app/(with-layout)/like-page/page.tsx
+++ src/app/(with-layout)/like-page/page.tsx
@@ -1,8 +1,20 @@
+import { Card } from '@/components'
+import RECOMMENDED from '@/mocks/main/recommended'
+
 const LikePage = () => {
   return (
-    <div>
-      <h1>Like Page</h1>
-    </div>
+    <section className="mx-auto my-25 w-full max-w-320">
+      <article className="flex h-full flex-col gap-16">
+        <h1 className="border-b-2 border-[#000] pb-5 text-2xl font-bold text-[#333]">
+          찜한 상품
+        </h1>
+        <div className="mx-auto grid grid-cols-4 gap-7">
+          {RECOMMENDED.map((item, index) => (
+            <Card key={index} type="product" data={item} />
+          ))}
+        </div>
+      </article>
+    </section>
   )
 }
 
