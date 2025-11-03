c9067c92790ba271f7a26e8c2f096881dfb75b45
han-nun <spshspshek@gmail.com>
2025-11-03 22:12:10 +0900

    refactor: 코드리뷰 반영 (#55)
    - 코드리뷰 반영

--- src/hooks/my-page/useEditPage.ts
+++ src/hooks/my-page/useEditPage.ts
@@ -1,15 +1,17 @@
 import { useState } from 'react'
 
-import { useEditPageModals } from './useEditPageModals'
 
-export const useEditPage = () => {
+type UseEditPageProps = {
+  openAlarmModal: () => void
+}
+
+export const useEditPage = ({ openAlarmModal }: UseEditPageProps) => {
   const [selected, setSelected] = useState('비동의')
-  const { alarmModal } = useEditPageModals()
-
+  
   const handleMarketingChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSelected(e.target.value)
     if (e.target.value === '동의') {
-      alarmModal.open()
+      openAlarmModal()
     }
   }
 
