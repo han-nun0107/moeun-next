3ea678ced8ee389dec56d38455ae9bfab855f27c
han-nun <spshspshek@gmail.com>
2025-11-09 17:14:53 +0900

    refactor: 코드 리뷰 반영 (#63)

--- src/components/common/modal/ReviewModal.tsx
+++ src/components/common/modal/ReviewModal.tsx
@@ -34,32 +34,32 @@ const ReviewModal = ({
       isOpen={isOpen}
       onClose={onClose}
       title="한 잔 취향 이 달의 후기"
-      className="review-modal-scroll h-[900px] w-170 overflow-x-hidden overflow-y-auto"
+      className="review-modal-scroll h-225 w-170 overflow-x-hidden overflow-y-auto"
     >
       <div>
         <Image
           src={imgSrc || IMAGE_URLS.Product.Default}
           alt={imgAlt || '모은 주류'}
-          className="mt-14 mb-[34px] rounded-[10px] border border-[#333333]"
+          className="border-black-200 mt-14 mb-[34px] rounded-[10px] border"
           width={600}
           height={476}
         />
-        <p className="pb-[4px] text-[40px]">{product_name}</p>
+        <p className="pb-1 text-[40px]">{product_name}</p>
         <StarRating
           totalStars={5}
           readOnly
           defaultRating={defaultRating}
           size={25}
           showRatingValue={false}
-          className="my-[23px]"
+          className="my-6"
         />
-        <p className="mb-15 w-140 text-[22px] text-[#333333]">{review}</p>
-        <div className="mb-[101px] flex w-[250px] justify-between text-[22px] text-[#666666]">
+        <p className="text-black-200 mb-15 w-140 text-[22px]">{review}</p>
+        <div className="mb-25 flex w-[250px] justify-between text-[22px] text-gray-700">
           <p> {userId ? maskingUserId(userId) : 'Unknown User'}</p>
           <p>{date ? date.slice(0, 10) : ''}</p>
         </div>
       </div>
-      <Link href={`/item/${String(product_id)}`}>
+      <Link href={`/item/${product_id}`}>
         <Button variant="FEEDBACK_SUBMIT">제품 상세보기</Button>
       </Link>
     </Modal>
