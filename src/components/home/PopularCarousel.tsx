0e5f6b9a39c97645384b90189d7bb3b0a41e4e8d
han-nun <spshspshek@gmail.com>
2025-12-23 22:55:48 +0900

    refactor: 코드 리뷰 반영 (#69)

--- src/components/home/PopularCarousel.tsx
+++ src/components/home/PopularCarousel.tsx
@@ -45,13 +45,10 @@ const PopularCarousel = () => {
               <Card
                 type="product"
                 data={{
-                  img:
-                    product.main_image_url ||
-                    product.images[0]?.image_url ||
-                    '',
+                  img: product.main_image_url || '',
                   alt: product.name,
                   title: product.name,
-                  subtitle: product.drink?.brewery?.name || '',
+                  subtitle: '',
                   price: product.final_price,
                 }}
               />
