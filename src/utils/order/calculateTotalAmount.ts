402336a4fe0a5ff2ae9bd921a94694402f7204eb
han-nun <spshspshek@gmail.com>
2025-12-30 23:52:16 +0900

    refactor: 코드 리팩토링 (#75)

--- src/utils/order/calculateTotalAmount.ts
+++ src/utils/order/calculateTotalAmount.ts
@@ -5,8 +5,8 @@ type CartItem = {
 
 export const calculateTotalAmount = (cartItems: CartItem[]): number => {
   return cartItems.reduce((sum, item) => {
-    const price = Number(item.price_at_added ?? 0)
-    const quantity = Number(item.quantity ?? 0)
+    const price = item.price_at_added || 0
+    const quantity = item.quantity
     return sum + price * quantity
   }, 0)
 }
